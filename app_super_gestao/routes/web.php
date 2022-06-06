7ca5fb2960b4002c251c9197536f60211d94ff2e
cristian-santos <cristiansantosti@gmail.com>
2022-06-05 22:11:41 -0300

    criando controllers

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,15 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá, seja bem-vindo ao curso';
-});
+// Route::get('/', function () {
+//     return 'Olá, seja bem-vindo ao curso';
+// });
 
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre nós';
-});
+Route::get('/', [PrincipalController::class, 'principal']);
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+
