8e75e8e06fd80fa6093bad1f1eae22a4688457ee
cristian-santos <cristiansantosti@gmail.com>
2022-06-06 06:49:48 -0300

    avançando nas rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -20,11 +20,21 @@ use Illuminate\Support\Facades\Route;
 //     return 'Olá, seja bem-vindo ao curso';
 // });
 
+// Route::get('contato/{nome}/{categoria}/{assunto}/{mensagem?}', 
+//     function(string $nome, string $categoria, string $assunto, string $mensagem = 'Mensagem não informada') {
+//         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+//     });
+
+Route::get('contato/{nome}/{categoria_id}', 
+    function(string $nome = 'Desconhecido',
+                int $categoria_id = 1) {
+        echo "Estamos aqui: $nome - $categoria_id";
+    })->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+
 
 Route::get('/', [PrincipalController::class, 'principal']);
 
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
-
 
