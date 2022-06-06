35cf031bd1ed58788171fd3962978eafa31a9645
cristian-santos <cristiansantosti@gmail.com>
2022-06-06 07:09:52 -0300

    rotas nomeadas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -25,16 +25,26 @@ use Illuminate\Support\Facades\Route;
 //         echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
 //     });
 
-Route::get('contato/{nome}/{categoria_id}', 
-    function(string $nome = 'Desconhecido',
-                int $categoria_id = 1) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    })->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+// Route::get('contato/{nome}/{categoria_id}', 
+//     function(string $nome = 'Desconhecido',
+//                 int $categoria_id = 1) {
+//         echo "Estamos aqui: $nome - $categoria_id";
+//     })->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
 
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+
+Route::get('/login', function(){return 'Login';})->name('site.login');
+
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
+
 
