05a36518fdd9c5d12345df4b7a776d06b1952b55
cristian-santos <cristiansantosti@gmail.com>
2022-06-16 11:04:21 -0300

    exibindo dados no blade com condições

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,7 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-
-        return view('app.fornecedor.index');
+        $fornecedores = ['Fornecedor 1'];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,13 @@
 <h3>Fornecedor</h3>
 
 {{-- Fica o comentário aqui dentro --}}
+
+{{--@dd($fornecedores)--}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
