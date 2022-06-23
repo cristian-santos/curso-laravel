f4fb954a0900176ccbd781c78ae9c6236c9a6a7d
cristian-santos <cristiansantosti@gmail.com>
2022-06-22 22:23:46 -0300

    migrations metodo down

--- app_super_gestao/database/migrations/2022_06_23_005628_alter_forncedores_novas_colunas.php
+++ app_super_gestao/database/migrations/2022_06_23_005628_alter_forncedores_novas_colunas.php
@@ -23,9 +23,15 @@ return new class extends Migration
      * Reverse the migrations.
      *
      * @return void
+     * Executar: php artisan migrate:rollback
+     * Executar: php artisan migrate:rollback --step=2
      */
     public function down()
     {
-        //
+        Schema::table('fornecedores', function (Blueprint $table) {
+        // $table->dropColumn('uf');
+        // $table->dropColumn('email');
+        $table->dropColumn('uf', 'email');
+        });
     }
 };
